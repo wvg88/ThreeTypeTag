7e93771b17a8fa0f211ca1a63d10847f7ec9f13a
wvg88 <woutervanginneken@gmail.com>
2022-04-25 16:34:37 +0200

    Dynamic JSON data

--- src/client/client.ts
+++ src/client/client.ts
@@ -3,9 +3,15 @@ import * as THREE from 'three'
 import { Object3D } from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
-const amount = 300
+let amount = 4
 const size = 5
 let particles: Object3D[] = []
+
+const data:object[] = []
+let INTERSECTED: any
+let ID:number
+let Content:any
+const points:any = []
 
 const scene = new THREE.Scene()
 scene.background = new THREE.Color( 0xcccccc )
@@ -17,7 +23,47 @@ const renderer = new THREE.WebGLRenderer()
 const container = document.getElementById('container')
 container?.appendChild(renderer.domElement)
 
+const UItext = document.createElement("div")
+UItext.setAttribute('id','textarea')
+UItext.innerHTML = "Hello"
+document.body.appendChild(UItext)
 
+const geometry = new THREE.BufferGeometry()
+const line = new THREE.Line( geometry, new THREE.LineBasicMaterial( { color: 0xe0ffe0 } ) )
+
+const url = window.location.href + 'data.json'
+
+async function getJsonData() {
+     let j = await (await fetch(url)).json()
+     //let stringified:string = j.members[0].name
+
+     for (let i in j.members) {
+         const element = j.members[i].name;
+         const particle = new THREE.Mesh(sg, material)
+         const point = new THREE.Vector3()
+         point.x = Math.random() * 8 - 1 
+         point.y = Math.random() * 4 - 1 
+         point.z = Math.random() * 2 - 1
+         point.normalize()
+         point.multiplyScalar( Math.random() * amount + 450)
+     
+         particle.position.x = point.x
+         particle.position.y = point.y
+         particle.position.z = point.z
+         
+         particle.scale.x = particle.scale.y = particle.scale.z = size
+         particles.push( particle )
+         
+         points.push(point)
+         scene.add (particle)
+         data.push(element)
+     }
+     geometry.setFromPoints(points)
+
+} 
+
+
+getJsonData()
 
 
 renderer.setPixelRatio( window.devicePixelRatio )
@@ -27,11 +73,10 @@ const controls = new OrbitControls(camera, renderer.domElement)
 const raycaster = new THREE.Raycaster()
 const mouse = new THREE.Vector2()
 
-let INTERSECTED: any
 
 
-const geometry = new THREE.BufferGeometry()
-const points = []
+
+
 
 const material = new THREE.MeshBasicMaterial({
     color: 0x000000,
@@ -44,27 +89,11 @@ sprite.scale.set(100,20,0)
 
 
 
-for ( var i =0; i < amount; i ++){
-    const particle = new THREE.Mesh(sg, material)
-    const point = new THREE.Vector3()
-    point.x = Math.random() * 8 - 1 
-    point.y = Math.random() * 4 - 1 
-    point.z = Math.random() * 2 - 1
-    point.normalize()
-    point.multiplyScalar( Math.random() * amount + 450)
-
-    particle.position.x = point.x
-    particle.position.y = point.y
-    particle.position.z = point.z
+for ( let i =0; i < amount; i ++){
+   
     
-    particle.scale.x = particle.scale.y = particle.scale.z = size
-    particles.push( particle )
-    
-    points.push(point)
-    scene.add (particle)
 }
-geometry.setFromPoints(points)
-const line = new THREE.Line( geometry, new THREE.LineBasicMaterial( { color: 0xe0ffe0 } ) )
+
 
 
 
@@ -86,64 +115,39 @@ function onWindowResize() {
 function animate() {
     requestAnimationFrame(animate)
     controls.update()
-
     render()
 }
 
 
-const UItext = document.createElement("div")
-UItext.setAttribute('id','textarea')
-document.body.appendChild(UItext)
-const url = window.location.href + 'data.json'
-let stringified:string
-let Content:any
-let intersectID:any
-
-async function getJsonData() {
-     let j = await (await fetch(url)).json()
-     stringified = j.name + ' ' + j.surname  
-     Content = document.createTextNode("Hello, " + stringified)
-} 
-getJsonData()
-
-
-
-
-
-
 function render() {
+    
     raycaster.setFromCamera( mouse, camera );
-
+    
     let intersects = raycaster.intersectObjects(particles, true)
+   
     if ( intersects.length > 0 ) {
+        
         if ( INTERSECTED != intersects[ 0 ].object ) {
-            if ( INTERSECTED ) INTERSECTED.scale.x = INTERSECTED.scale.y = INTERSECTED.scale.z = size
+            if ( INTERSECTED ) INTERSECTED.scale.x = INTERSECTED.scale.y = INTERSECTED.scale.z = size 
             INTERSECTED = intersects[ 0 ].object;
-            console.log(INTERSECTED.id)
-            INTERSECTED.scale.x = INTERSECTED.scale.y = INTERSECTED.scale.z = size * 3
-            UItext.appendChild(Content)
 
-            
+            //weird
+            ID = INTERSECTED.id-12
+            console.log(ID)
+            UItext.innerHTML = JSON.stringify(data[ID])
+
+            INTERSECTED.scale.x = INTERSECTED.scale.y = INTERSECTED.scale.z = size * 3
             
             UItext.style.visibility = "visible"
-
-
-            //sprite.position.set( INTERSECTED.position.x +50,INTERSECTED.position.y+20,INTERSECTED.position.z)
-            //scene.add(sprite)
             scene.add(line)
-
-
-           
         }
+        
     }else{
         if ( INTERSECTED ) INTERSECTED.scale.x = INTERSECTED.scale.y = INTERSECTED.scale.z = size 
+            INTERSECTED = null
             
-            INTERSECTED = undefined
-            //scene.remove(sprite)
-           
-        UItext.style.visibility = "hidden"
-        scene.remove(line)
-       
+            UItext.style.visibility = "hidden"
+            scene.remove(line)
     }
     renderer.render(scene, camera)
 }
